08f6c5ae516a152182518d7324e0725029d5ed06
Qiangx7 <62750920+QZx7@users.noreply.github.com>
2022-07-26 12:01:12 +0900

    update generate length

---
+++ src/joint_learning.py
@@ -0,0 +1,24 @@
+# implement blenderbot here
+from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
+
+mname = "facebook/blenderbot-400M-distill"
+model = BlenderbotForConditionalGeneration.from_pretrained(mname)
+tokenizer = BlenderbotTokenizer.from_pretrained(mname)
+UTTERANCE = "My friends are cool but they eat too many carbs."
+print("Human: ", UTTERANCE)
+
+inputs = tokenizer([UTTERANCE], return_tensors="pt")
+reply_ids = model.generate(**inputs)
+print("Bot: ", tokenizer.batch_decode(reply_ids, skip_special_tokens=True)[0])
+
+REPLY = "I'm not sure"
+print("Human: ", REPLY)
+
+NEXT_UTTERANCE = (
+    "My friends are cool but they eat too many carbs.</s> <s>That's unfortunate. "
+    "Are they trying to lose weight or are they just trying to be healthier?</s> "
+    "<s> I'm not sure."
+)
+inputs = tokenizer([NEXT_UTTERANCE], return_tensors="pt")
+next_reply_ids = model.generate(**inputs)
+print("Bot: ", tokenizer.batch_decode(next_reply_ids, skip_special_tokens=True)[0])
