c68b131246a57f2322162eeabf63fdbf432d987f
Qiangx7 <62750920+QZx7@users.noreply.github.com>
2022-08-22 14:12:33 +0900

    Update prompting.py

--- bbmhr/pipeline/prompting.py
+++ bbmhr/pipeline/prompting.py
@@ -229,7 +229,7 @@ def load_large_model(model_name: Text):
         model = AutoModelForCausalLM.from_pretrained(model_name)
         tokenizer = AutoTokenizer.from_pretrained(model_name)
     elif "gpt-2" == model_name:
-        model_name = "distilgpt2"
+        model_name = "gpt2"
         print(f"loading model from {model_name}")
         tokenizer = GPT2Tokenizer.from_pretrained(model_name)
         model = GPT2LMHeadModel.from_pretrained(model_name)
