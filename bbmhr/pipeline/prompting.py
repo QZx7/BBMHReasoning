00daf00573503c4f36e62f06623e458a381111c8
Qiangx7 <62750920+QZx7@users.noreply.github.com>
2022-09-12 15:19:31 +0900

    Update prompting.py

--- bbmhr/pipeline/prompting.py
+++ bbmhr/pipeline/prompting.py
@@ -526,6 +526,7 @@ def inference(model_name, model, tokenizer, prompt_template: Text, current_dialo
     response = ""
     if model_name in ["gpt", "gpt-2"]:
         response = gpt_text_generate(prompt.strip(), model, tokenizer)
+        print(response)
         response = response.split("In this conversation,")[-1].split(":")[0].replace("\nsupporter", "").replace("\nConversation", "")
     elif model_name in ["ada", "davinci"]:
         response = get_gpt_result("completion", prompt, stop_words=['\n'], model_type=model_name)
