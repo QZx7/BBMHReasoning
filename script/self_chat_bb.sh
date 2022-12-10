c738462b39a57e04a81935006665474e1aec81ef
Qiangx7 <akitohisano@foxmail.com>
2022-12-10 11:11:23 +0900

    update incontext conversation

--- script/self_chat_bb.sh
+++ script/self_chat_bb.sh
@@ -2,89 +2,89 @@
 python3 bbmhr/parlai/scripts/self_chat_mental.py \
 --model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
 --task blended_skill_talk \
---num-self-chats 1 \
+--num-self-chats 20 \
 --selfchat-max-turns 20 \
 --partner-model-file ~/dialogue_base/blenderbot/model/bbmh/27B/bbmh_27B \
 --outfile ./eval/self_chat/bbmh_academic.txt \
 --seed-messages-from-file ./eval/self_chat/seeds/seed_2/academic.txt
 
-python3 bbmhr/parlai/scripts/self_chat_mental.py \
---model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
---task blended_skill_talk \
---num-self-chats 1 \
---selfchat-max-turns 20 \
---partner-model-file ~/dialogue_base/blenderbot/model/bbmh/27B/bbmh_27B \
---outfile ./eval/self_chat/bbmh_job.txt \
---seed-messages-from-file ./eval/self_chat/seeds/seed_2/job.txt
+# python3 bbmhr/parlai/scripts/self_chat_mental.py \
+# --model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
+# --task blended_skill_talk \
+# --num-self-chats 1 \
+# --selfchat-max-turns 20 \
+# --partner-model-file ~/dialogue_base/blenderbot/model/bbmh/27B/bbmh_27B \
+# --outfile ./eval/self_chat/bbmh_job.txt \
+# --seed-messages-from-file ./eval/self_chat/seeds/seed_2/job.txt
 
-python3 bbmhr/parlai/scripts/self_chat_mental.py \
---model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
---task blended_skill_talk \
---num-self-chats 1 \
---selfchat-max-turns 20 \
---partner-model-file ~/dialogue_base/blenderbot/model/bbmh/27B/bbmh_27B \
---outfile ./eval/self_chat/bbmh_friend.txt \
---seed-messages-from-file ./eval/self_chat/seeds/seed_2/friend.txt
+# python3 bbmhr/parlai/scripts/self_chat_mental.py \
+# --model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
+# --task blended_skill_talk \
+# --num-self-chats 1 \
+# --selfchat-max-turns 20 \
+# --partner-model-file ~/dialogue_base/blenderbot/model/bbmh/27B/bbmh_27B \
+# --outfile ./eval/self_chat/bbmh_friend.txt \
+# --seed-messages-from-file ./eval/self_chat/seeds/seed_2/friend.txt
 
-python3 bbmhr/parlai/scripts/self_chat_mental.py \
---model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
---task blended_skill_talk \
---num-self-chats 1 \
---selfchat-max-turns 20 \
---partner-model-file ~/dialogue_base/blenderbot/model/bbmh/27B/bbmh_27B \
---outfile ./eval/self_chat/bbmh_ongoing.txt \
---seed-messages-from-file ./eval/self_chat/seeds/seed_2/ongoing.txt
+# python3 bbmhr/parlai/scripts/self_chat_mental.py \
+# --model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
+# --task blended_skill_talk \
+# --num-self-chats 1 \
+# --selfchat-max-turns 20 \
+# --partner-model-file ~/dialogue_base/blenderbot/model/bbmh/27B/bbmh_27B \
+# --outfile ./eval/self_chat/bbmh_ongoing.txt \
+# --seed-messages-from-file ./eval/self_chat/seeds/seed_2/ongoing.txt
 
-python3 bbmhr/parlai/scripts/self_chat_mental.py \
---model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
---task blended_skill_talk \
---num-self-chats 1 \
---selfchat-max-turns 20 \
---partner-model-file ~/dialogue_base/blenderbot/model/bbmh/27B/bbmh_27B \
---outfile ./eval/self_chat/bbmh_partner.txt \
---seed-messages-from-file ./eval/self_chat/seeds/seed_2/partner.txt
+# python3 bbmhr/parlai/scripts/self_chat_mental.py \
+# --model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
+# --task blended_skill_talk \
+# --num-self-chats 1 \
+# --selfchat-max-turns 20 \
+# --partner-model-file ~/dialogue_base/blenderbot/model/bbmh/27B/bbmh_27B \
+# --outfile ./eval/self_chat/bbmh_partner.txt \
+# --seed-messages-from-file ./eval/self_chat/seeds/seed_2/partner.txt
 
-python3 bbmhr/parlai/scripts/self_chat_mental.py \
---model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
---task blended_skill_talk \
---num-self-chats 1 \
---selfchat-max-turns 20 \
---partner-model-file zoo:blender/blender_3B/model \
---outfile ./eval/self_chat/bb_academic.txt \
---seed-messages-from-file ./eval/self_chat/seeds/seed_2/academic.txt
+# python3 bbmhr/parlai/scripts/self_chat_mental.py \
+# --model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
+# --task blended_skill_talk \
+# --num-self-chats 1 \
+# --selfchat-max-turns 20 \
+# --partner-model-file zoo:blender/blender_3B/model \
+# --outfile ./eval/self_chat/bb_academic.txt \
+# --seed-messages-from-file ./eval/self_chat/seeds/seed_2/academic.txt
 
-python3 bbmhr/parlai/scripts/self_chat_mental.py \
---model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
---task blended_skill_talk \
---num-self-chats 1 \
---selfchat-max-turns 20 \
---partner-model-file zoo:blender/blender_3B/model \
---outfile ./eval/self_chat/bb_job.txt \
---seed-messages-from-file ./eval/self_chat/seeds/seed_2/job.txt
+# python3 bbmhr/parlai/scripts/self_chat_mental.py \
+# --model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
+# --task blended_skill_talk \
+# --num-self-chats 1 \
+# --selfchat-max-turns 20 \
+# --partner-model-file zoo:blender/blender_3B/model \
+# --outfile ./eval/self_chat/bb_job.txt \
+# --seed-messages-from-file ./eval/self_chat/seeds/seed_2/job.txt
 
-python3 bbmhr/parlai/scripts/self_chat_mental.py \
---model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
---task blended_skill_talk \
---num-self-chats 1 \
---selfchat-max-turns 20 \
---partner-model-file zoo:blender/blender_3B/model \
---outfile ./eval/self_chat/bb_friend.txt \
---seed-messages-from-file ./eval/self_chat/seeds/seed_2/friend.txt
+# python3 bbmhr/parlai/scripts/self_chat_mental.py \
+# --model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
+# --task blended_skill_talk \
+# --num-self-chats 1 \
+# --selfchat-max-turns 20 \
+# --partner-model-file zoo:blender/blender_3B/model \
+# --outfile ./eval/self_chat/bb_friend.txt \
+# --seed-messages-from-file ./eval/self_chat/seeds/seed_2/friend.txt
 
-python3 bbmhr/parlai/scripts/self_chat_mental.py \
---model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
---task blended_skill_talk \
---num-self-chats 1 \
---selfchat-max-turns 20 \
---partner-model-file zoo:blender/blender_3B/model \
---outfile ./eval/self_chat/bb_ongoing.txt \
---seed-messages-from-file ./eval/self_chat/seeds/seed_2/ongoing.txt
+# python3 bbmhr/parlai/scripts/self_chat_mental.py \
+# --model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
+# --task blended_skill_talk \
+# --num-self-chats 1 \
+# --selfchat-max-turns 20 \
+# --partner-model-file zoo:blender/blender_3B/model \
+# --outfile ./eval/self_chat/bb_ongoing.txt \
+# --seed-messages-from-file ./eval/self_chat/seeds/seed_2/ongoing.txt
 
-python3 bbmhr/parlai/scripts/self_chat_mental.py \
---model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
---task blended_skill_talk \
---num-self-chats 1 \
---selfchat-max-turns 20 \
---partner-model-file zoo:blender/blender_3B/model \
---outfile ./eval/self_chat/bb_partner.txt \
---seed-messages-from-file ./eval/self_chat/seeds/seed_2/partner.txt
+# python3 bbmhr/parlai/scripts/self_chat_mental.py \
+# --model-file ~/dialogue_base/blenderbot/model/bbmh/seeker/bbmh_27B \
+# --task blended_skill_talk \
+# --num-self-chats 1 \
+# --selfchat-max-turns 20 \
+# --partner-model-file zoo:blender/blender_3B/model \
+# --outfile ./eval/self_chat/bb_partner.txt \
+# --seed-messages-from-file ./eval/self_chat/seeds/seed_2/partner.txt
